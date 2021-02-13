18081b6beb902d23ff0f2f83447b279a95461620
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-02-13 22:14:41 +0530

    Add project files.

---
+++ TestClass.cs
@@ -0,0 +1,28 @@
+﻿// NUnit 3 tests
+
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTutorials
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement EmailTextField = driver.FindElement(By.XPath("//*[@id='email']"));
+            EmailTextField.SendKeys("Selenium c#");
+            driver.Quit();
+        }
+
+
+    }
+}
