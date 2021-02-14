e7d65f0ae5dc0aa8700880f5dfacf881f7abea91
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-02-14 21:11:59 +0530

    Running more tests at once. Inherit a base Test class and reducing the lines of coding.

---
+++ BaseClass/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTutorials.BaseClass
+{
+    public class BaseTest
+    {
+        public IWebDriver driver;
+        [OneTimeSetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com/";
+        }
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+    }
+}
--- TestClass.cs
+++ TestClass.cs
@@ -5,23 +5,48 @@ using System.Collections;
 using System.Collections.Generic;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniumTutorials.BaseClass;
+using System.Threading;
 
 namespace SeleniumTutorials
 {
     [TestFixture]
-    public class TestClass
+    public class TestClass : BaseTest
     {
         [Test]
-        public void TestMethod()
+        public void TestMethod1()
         {
             // TODO: Add your test code here
             
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://www.facebook.com/";
+            
             IWebElement EmailTextField = driver.FindElement(By.XPath("//*[@id='email']"));
             EmailTextField.SendKeys("Selenium c#");
-            driver.Quit();
+            
         }
+
+        [Test]
+        public void TestMethod2()
+        {
+            // TODO: Add your test code here
+
+
+            IWebElement EmailTextField = driver.FindElement(By.XPath("//*[@id='email']"));
+            EmailTextField.SendKeys("Selenium c#");
+
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            // TODO: Add your test code here
+
+
+            IWebElement EmailTextField = driver.FindElement(By.XPath("//*[@id='email']"));
+            EmailTextField.SendKeys("Selenium c#");
+            Thread.Sleep(3000);
+
+        }
+
 
 
     }
