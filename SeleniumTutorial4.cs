6e7f2dd24a06610cd367360bde26c9f7ed94905c
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-02-21 15:53:15 +0530

    Testing three different web sites with same test case including the Author, Description, Test case source attributes.

---
+++ SeleniumTutorial4.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTutorials
+{
+    public class SeleniumTutorial4
+    {
+        [Test]
+        [Author("Thisal", "thisal@quadrate.lk")]
+        [Description("Facebook login verified")]
+        [TestCaseSource("DataDrivenTesting")]
+        public void Test1(String urlName)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            //driver.Url = "https://www.facebook.com/";
+            driver.Url = urlName;
+            //IWebElement EmailTextField = driver.FindElement(By.XPath("//*[@id='email']"));
+            //EmailTextField.SendKeys("Selenium c#");
+            driver.Quit();
+        }
+
+        static IList DataDrivenTesting()
+        {
+            ArrayList list = new ArrayList();
+            list.Add("https://www.facebook.com/");
+            list.Add("https://www.youtube.com/");
+            list.Add("https://www.gmail.com/");
+            return list;
+        }
+
+/*
+        [Test]
+        [Author("Thisal", "thisal@quadrate.lk")]
+        [Description("Facebook login verified")]
+        public void Test2()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement EmailTextField = driver.FindElement(By.XPath("//*[@id='email']"));
+            EmailTextField.SendKeys("Selenium c#");
+            driver.Quit();
+        }*/
+    }
+}
